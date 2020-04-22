c0f90b7a4168db28d326402d75eb92e73df0b96f
rijadukic <rijadukic@rarecrew.com>
2020-04-22 09:02:30 +0200

    Ticket #ArticleSearch-0001: Added Project to github

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,55 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Net.Http.Headers;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Mvc;
+using WebApplication4.Models;
+
+namespace WebApplication4.Controllers
+{
+    [Authorize]
+    public class HomeController : Controller
+    {
+     
+        public  ActionResult Index()
+        {
+            return  View();
+        }
+        [HttpPost]
+        public async Task<ActionResult> ArticlesList(VerifyMainSearchInputViewModel searchingString)
+        {
+            if (ModelState.IsValid)
+            {
+
+                var table = new VerifyMainSearchInputViewModel();
+                string url =   "https://api.nytimes.com/svc/search/v2/articlesearch.json?q=" + searchingString.inputView + "&api-key=iV0re7Z4zTAccdORNq8JEANgkqntbE5A";
+                      using (System.Net.Http.HttpClient client = new System.Net.Http.HttpClient())
+                {
+                    client.BaseAddress = new Uri(url);
+                    client.DefaultRequestHeaders.Accept.Clear();
+                    client.DefaultRequestHeaders.Accept.Add(new
+                    MediaTypeWithQualityHeaderValue("application/json"));
+
+                    System.Net.Http.HttpResponseMessage response = await client.GetAsync(url);
+                    if (response.IsSuccessStatusCode)
+                    {
+                        var data = await response.Content.ReadAsStringAsync();
+                        table = JsonConvert.DeserializeObject<VerifyMainSearchInputViewModel>(data);
+
+
+                    }
+                }
+                return PartialView("_Articles", table);
+            }
+
+            return View("Error.cshml");
+
+
+        }
+    }
+}
---
+++ Models/ManageViewModels.cs
@@ -0,0 +1,115 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Web.Helpers;
+using Microsoft.AspNet.Identity;
+using Microsoft.Owin.Security;
+
+namespace WebApplication4.Models
+{
+    public class IndexViewModel
+    {
+        public bool HasPassword { get; set; }
+        public IList<UserLoginInfo> Logins { get; set; }
+        public string PhoneNumber { get; set; }
+        public bool TwoFactor { get; set; }
+        public bool BrowserRemembered { get; set; }
+    }
+
+    public class ManageLoginsViewModel
+    {
+        public IList<UserLoginInfo> CurrentLogins { get; set; }
+        public IList<AuthenticationDescription> OtherLogins { get; set; }
+    }
+
+    public class FactorViewModel
+    {
+        public string Purpose { get; set; }
+    }
+
+    public class SetPasswordViewModel
+    {
+        [Required]
+        [StringLength(100, ErrorMessage = "The {0} must be at least {2} characters long.", MinimumLength = 6)]
+        [DataType(DataType.Password)]
+        [Display(Name = "New password")]
+        public string NewPassword { get; set; }
+
+        [DataType(DataType.Password)]
+        [Display(Name = "Confirm new password")]
+        [Compare("NewPassword", ErrorMessage = "The new password and confirmation password do not match.")]
+        public string ConfirmPassword { get; set; }
+    }
+
+    public class ChangePasswordViewModel
+    {
+        [Required]
+        [DataType(DataType.Password)]
+        [Display(Name = "Current password")]
+        public string OldPassword { get; set; }
+
+        [Required]
+        [StringLength(100, ErrorMessage = "The {0} must be at least {2} characters long.", MinimumLength = 6)]
+        [DataType(DataType.Password)]
+        [Display(Name = "New password")]
+        public string NewPassword { get; set; }
+
+        [DataType(DataType.Password)]
+        [Display(Name = "Confirm new password")]
+        [Compare("NewPassword", ErrorMessage = "The new password and confirmation password do not match.")]
+        public string ConfirmPassword { get; set; }
+    }
+
+    public class AddPhoneNumberViewModel
+    {
+        [Required]
+        [Phone]
+        [Display(Name = "Phone Number")]
+        public string Number { get; set; }
+    }
+
+    public class VerifyPhoneNumberViewModel
+    {
+        [Required]
+        [Display(Name = "Code")]
+        public string Code { get; set; }
+
+        [Required]
+        [Phone]
+        [Display(Name = "Phone Number")]
+        public string PhoneNumber { get; set; }
+    }
+
+    public class ConfigureTwoFactorViewModel
+    {
+        public string SelectedProvider { get; set; }
+        public ICollection<System.Web.Mvc.SelectListItem> Providers { get; set; }
+    }
+    public class VerifyMainSearchInputViewModel
+    {
+        [Display(Name = "response")]
+        public DocsDetail response { get; set; }
+
+
+        [Required(ErrorMessage = "Search Field is Mandatory")]
+        [RegularExpression(@".*\S+.*$", ErrorMessage = "Field Cannot be Blank Or Whitespace")]
+        public string inputView { get; set; }
+    }
+
+    public class DocsDetail
+    {
+        public List<DocsDetails> docs { get; set; }
+    }
+    public class DocsDetails
+    {
+        public string source { get; set; }
+        public string document_type { get; set; }
+
+        public string section_name { get; set; }
+        public Headline headline { get; set; }
+    }
+    public class Headline
+    {
+        public string main { get; set; }
+    }
+
+}
